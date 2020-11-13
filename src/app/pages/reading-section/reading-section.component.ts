c6f1883dc8c1cfb18b9c39f3c0961bd1ada76231
Sukhdev Patidar <sukhdevpatidar@Sukhdevs-MacBook-Air.local>
2020-11-13 12:30:29 +0530

    added darg drop

--- src/app/pages/reading-section/reading-section.component.ts
+++ src/app/pages/reading-section/reading-section.component.ts
@@ -17,20 +17,6 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./reading-section.component.scss']
 })
 export class ReadingSectionComponent implements OnInit {
-  todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
-
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
 
   EnumService = EnumService;
 
@@ -108,6 +94,15 @@ export class ReadingSectionComponent implements OnInit {
       // {"examSectionId":17,"sessionId":"b34ab0e5-ec3e-4c13-9ae5-556a9f07439c","examTypeId":1,"examTypeText":"SAT","title":"SAT-EXAM-01","description":"<p>SAT-EXAM-01</p>","duration":null,"sectionData":{"id":1,"name":"Reading","guideline":null,"sets":[{"id":2,"orderNo":1,"passage":" ","videoUri":null,"videoRepeatable":false,"questions":[{"id":1,"typeId":1,"text":"<p>q1x</p>","imageUri":"/medialibrary/image/worldmap.jpg","orderNo":0,"weight":1.00,"choices":[{"id":1,"text":"c1x","imageUri":null},{"id":2,"text":"c2x","imageUri":null},{"id":3,"text":"c3x","imageUri":null},{"id":4,"text":"c4x","imageUri":null}],"groups":[],"items":[]},{"id":10,"typeId":1,"text":" ","imageUri":null,"orderNo":1,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":12,"typeId":1,"text":" ","imageUri":null,"orderNo":2,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":17,"typeId":1,"text":"<p>aaaa</p>","imageUri":null,"orderNo":3,"weight":1.00,"choices":[{"id":20,"text":"cc1","imageUri":null},{"id":21,"text":"cc2","imageUri":null},{"id":22,"text":"cc3","imageUri":null},{"id":23,"text":"cc4","imageUri":null}],"groups":[],"items":[]},{"id":18,"typeId":1,"text":" ","imageUri":null,"orderNo":4,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":19,"typeId":1,"text":" ","imageUri":null,"orderNo":5,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":22,"typeId":1,"text":"<p>bbbb</p>","imageUri":null,"orderNo":6,"weight":1.00,"choices":[{"id":24,"text":"bc1","imageUri":null},{"id":25,"text":"bc2","imageUri":null},{"id":26,"text":"bc3","imageUri":null}],"groups":[],"items":[]},{"id":23,"typeId":1,"text":" ","imageUri":null,"orderNo":7,"weight":1.00,"choices":[],"groups":[],"items":[]}]}]},"isSuccess":true,"exception":null,"messages":["OK"]}
       this.examSessionData = JSON.parse(examSessionData);
       this.examSectionSets = this.examSessionData.sectionData?.sets;
+      this.examSectionSets.map((set) => {
+        set.questions.map((question) => {
+          if (question.typeId === EnumService.examQuestionTypes.DRAG_DROP) {
+            question.groups.map((group) => {
+              group.answered = [];
+            });
+          }
+        });
+      });
     }
 
     this.route.queryParams.subscribe((params) => {
@@ -120,7 +115,7 @@ export class ReadingSectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<string[]>): void {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -157,8 +152,29 @@ export class ReadingSectionComponent implements OnInit {
         } else if (question.typeId === EnumService.examQuestionTypes.VERIFIABLE_TEXT_MULTI_LINE || question.typeId === EnumService.examQuestionTypes.UNVERIFIABLE_TEXT_MULTI_LINE) {
           answerObject.answerInput = question.answerInput;
           isAsnwered = true;
-        }
+        } else if (question.typeId === EnumService.examQuestionTypes.DRAG_DROP) {
+          const groups = question.groups;
+          const groupItemMatches = [];
+          groups.map((group) => {
+            const answered = group.answered;
+            const answerIds = [];
+            answered.map((answer) => {
+              answerIds.push(answer.id);
+              isAsnwered = true;
+            });
 
+            const DragAndDropAnswerObject = {
+              questionId: group.id,
+              itemIds: answerIds
+            };
+
+            groupItemMatches.push(DragAndDropAnswerObject);
+
+          });
+          answerObject.groupItemMatches = groupItemMatches;
+        }
+        // resume {"examSectionId":59,"sessionId":"8aad755e-dddf-4cd2-9249-1f89a300e2ab","examTypeId":2,"examTypeText":"TOEFL","title":"TOEFL-UTKU01","description":"<p>TOEFL-UTKU01</p>","duration":null,"sectionData":{"id":1,"name":"Reading","guideline":"<p>TOEFL-UTKU01 Reading Guideline</p>","sets":[{"id":30,"orderNo":1,"passage":"<p>TOEFL-UTKU01 Reading Set 1 Passage</p>","videoUri":null,"videoRepeatable":false,"questions":[{"id":75,"typeId":1,"text":"<p>TOEFL-UTKU01 Reading Set 1 MCSS Question, Answer : A</p>","imageUri":null,"orderNo":1,"weight":1.70,"choices":[{"id":123,"text":"TOEFL-UTKU01 Reading Set 1 MCSS Choice A","imageUri":null},{"id":124,"text":"TOEFL-UTKU01 Reading Set 1 MCSS Choice B","imageUri":null},{"id":125,"text":"TOEFL-UTKU01 Reading Set 1 MCSS Choice C","imageUri":null},{"id":126,"text":"TOEFL-UTKU01 Reading Set 1 MCSS Choice C","imageUri":null}],"groups":[],"items":[]},{"id":76,"typeId":5,"text":"<p>TOEFL-UTKU01 Reading Set 1 MCMS Question, Answer : A/B</p>","imageUri":null,"orderNo":2,"weight":1.40,"choices":[{"id":127,"text":"TOEFL-UTKU01 Reading Set 1 MCMS Option A","imageUri":null},{"id":128,"text":"TOEFL-UTKU01 Reading Set 1 MCMS Option B","imageUri":null},{"id":129,"text":"TOEFL-UTKU01 Reading Set 1 MCMS Option C","imageUri":null},{"id":130,"text":"TOEFL-UTKU01 Reading Set 1 MCMS Option D","imageUri":null}],"groups":[],"items":[]},{"id":77,"typeId":8,"text":"<p>Match legends with their clubs.</p><p>TOEFL-UTKU01 Reading Set 1 D&amp;D Question, Answer :</p><p>Galatasaray :&nbsp;Gheorge Hagi,&nbsp;Hakan Ş&uuml;k&uuml;r,&nbsp;Wesley Sneijder</p><p>Fenerbah&ccedil;e :&nbsp;Roberto Carlos,&nbsp;Mateja Kezman,&nbsp;Rıdvan Dilmen</p>","imageUri":null,"orderNo":3,"weight":2.10,"choices":[],"groups":[{"id":1,"orderNo":0,"text":"Options that do not belong to any group"},{"id":2,"orderNo":1,"text":"Galatasaray"},{"id":3,"orderNo":2,"text":"Fenerbahçe"}],"items":[{"id":1,"text":"Leonel Messi"},{"id":2,"text":"Cristiano Ronaldo"},{"id":3,"text":"Luis Figo"},{"id":4,"text":"Wesley Sneijder"},{"id":5,"text":"Hakan Şükür"},{"id":6,"text":"Gheorge Hagi"},{"id":7,"text":"Rıdvan Dilmen"},{"id":8,"text":"Mateja Kezman"},{"id":9,"text":"Roberto Carlos"}]}]}]},"isSuccess":true,"exception":null,"messages":["OK"]}
+        // submited {"answers":[{"questionId":75,"selectedChoices":[123]},{"questionId":76,"selectedChoices":[127]},{"questionId":77,"groupItemMatches":[{"questionId":1,"itemIds":[4]},{"questionId":2,"itemIds":[1,3]},{"questionId":3,"itemIds":[2,5]}]}],"examSectionId":59}
         if (isAsnwered) {
           asnwers.push(answerObject);
         }
@@ -173,7 +189,6 @@ export class ReadingSectionComponent implements OnInit {
       this.loading = false;
       if (res.isSuccess) {
         this.openWritingSectionDialog();
-
       } else {
         this.alertService.error(res.messages.join('\n'));
       }
@@ -207,6 +222,8 @@ export class ReadingSectionComponent implements OnInit {
       console.log(`Dialog result: ${result}`);
       if (result) {
         this.endExamSession();
+      } else {
+        this.currentIndex = 0;
       }
     });
   }
--- src/app/pages/writing-section/writing-section.component.ts
+++ src/app/pages/writing-section/writing-section.component.ts
@@ -9,6 +9,7 @@ import {CookieService} from 'ngx-cookie-service';
 import {AccountService} from '../../services/account.service';
 import {ApiService} from '../../services/api.service';
 import {AlertService} from '../../services/alert.service';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-writing-section',
@@ -93,6 +94,15 @@ export class WritingSectionComponent implements OnInit {
       // {"examSectionId":17,"sessionId":"b34ab0e5-ec3e-4c13-9ae5-556a9f07439c","examTypeId":1,"examTypeText":"SAT","title":"SAT-EXAM-01","description":"<p>SAT-EXAM-01</p>","duration":null,"sectionData":{"id":1,"name":"Reading","guideline":null,"sets":[{"id":2,"orderNo":1,"passage":" ","videoUri":null,"videoRepeatable":false,"questions":[{"id":1,"typeId":1,"text":"<p>q1x</p>","imageUri":"/medialibrary/image/worldmap.jpg","orderNo":0,"weight":1.00,"choices":[{"id":1,"text":"c1x","imageUri":null},{"id":2,"text":"c2x","imageUri":null},{"id":3,"text":"c3x","imageUri":null},{"id":4,"text":"c4x","imageUri":null}],"groups":[],"items":[]},{"id":10,"typeId":1,"text":" ","imageUri":null,"orderNo":1,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":12,"typeId":1,"text":" ","imageUri":null,"orderNo":2,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":17,"typeId":1,"text":"<p>aaaa</p>","imageUri":null,"orderNo":3,"weight":1.00,"choices":[{"id":20,"text":"cc1","imageUri":null},{"id":21,"text":"cc2","imageUri":null},{"id":22,"text":"cc3","imageUri":null},{"id":23,"text":"cc4","imageUri":null}],"groups":[],"items":[]},{"id":18,"typeId":1,"text":" ","imageUri":null,"orderNo":4,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":19,"typeId":1,"text":" ","imageUri":null,"orderNo":5,"weight":1.00,"choices":[],"groups":[],"items":[]},{"id":22,"typeId":1,"text":"<p>bbbb</p>","imageUri":null,"orderNo":6,"weight":1.00,"choices":[{"id":24,"text":"bc1","imageUri":null},{"id":25,"text":"bc2","imageUri":null},{"id":26,"text":"bc3","imageUri":null}],"groups":[],"items":[]},{"id":23,"typeId":1,"text":" ","imageUri":null,"orderNo":7,"weight":1.00,"choices":[],"groups":[],"items":[]}]}]},"isSuccess":true,"exception":null,"messages":["OK"]}
       this.examSessionData = JSON.parse(examData);
       this.examSectionSets = this.examSessionData.sectionData?.sets;
+      this.examSectionSets.map((set) => {
+        set.questions.map((question) => {
+          if (question.typeId === EnumService.examQuestionTypes.DRAG_DROP) {
+            question.groups.map((group) => {
+              group.answered = [];
+            });
+          }
+        });
+      });
     }
 
     this.route.queryParams.subscribe((params) => {
@@ -103,6 +113,17 @@ export class WritingSectionComponent implements OnInit {
 
 
   ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
   }
 
   endExamSession = () => {
@@ -182,6 +203,8 @@ export class WritingSectionComponent implements OnInit {
       console.log(`Dialog result: ${result}`);
       if (result) {
         this.endExamSession();
+      } else {
+        this.currentIndex = 0;
       }
     });
   }
